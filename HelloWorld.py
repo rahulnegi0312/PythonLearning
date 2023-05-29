cd518155f1c8be27298a1bf93c3a1be498e22e9a
rahulnegi0312 <rahulnegi0312@gmail.com>
2023-05-29 20:37:49 +0530

    Notepad open code added

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,5 +1,10 @@
 import os
+import subprocess as sp
 print('Hello world')
 print('Rahul')
-command="python3 --version"
-os.system("gnome-terminal -e 'bash -c \""+command+";bash\"'")
+#command="python3 --version"
+#os.system("gnome-terminal -e 'bash -c \""+command+";bash\"'")
+prog="Notepad.exe"
+file="hello.txt"
+
+sp.Popen([prog,file])
